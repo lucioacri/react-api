1594819a22b711792a47361c73e47eeed5a51fe7
lucioacri <lucioacri@gmail.com>
2025-05-28 13:38:33 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [actressesArray, setActressesArray] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://lanciweb.github.io/demo/api/actresses/")
+      .then((response) => {
+        setActressesArray(response.data);
+      });
+  }, []);
+  return (
+    <ul>
+      {actressesArray.map((actress, index) => (
+        <li key={index}>{actress.name}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default App;
